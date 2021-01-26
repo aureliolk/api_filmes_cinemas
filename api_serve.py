d33cb29f9670237a60b95d0c35b612cb52251c0c
Aurélio Castro <aurelio.cos@outlook.com>
2021-01-26 17:12:59 -0300

    Fix add port Heroku

--- api_serve.py
+++ api_serve.py
@@ -44,6 +44,6 @@ def filmes():
 	return jsonify({'filmes': data})	
 
 if __name__ == "__main__":
-	port = int(os.environ.get('PORT',5000))
-	app.run(host = '0.0.0.0', port = port)
+	port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
  
