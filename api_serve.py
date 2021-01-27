0c05ac688afb897699c500815c08282fddcab5a4
Aurélio Castro <aurelio.cos@outlook.com>
2021-01-27 10:20:35 -0300

    reset function filmes

--- api_serve.py
+++ api_serve.py
@@ -7,6 +7,7 @@ import os
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
+url_target = "http://www.adorocinema.com/filmes/melhores/"
 
 @app.route('/',methods=['GET'])
 def home():
@@ -14,23 +15,22 @@ def home():
 
 @app.route('/api/filmes', methods=['GET'])
 def filmes():
-	url_target = "http://www.adorocinema.com/filmes/melhores/"
 	html_doc = urlopen(url_target)
 	soup = BeautifulSoup(html_doc,"html.parser")
-	return 'Load'
-	# data = []
 
-	# for dataBox in soup.find_all('div',class_='card entity-card entity-card-list cf'):
-	# 	titleObg = dataBox.find('h2',class_='meta-title').find('a',class_='meta-title-link').text
-	# 	durationObj = dataBox.find('div',class_='meta-body-item meta-body-info')
-	# 	img_cont = soup.find('img',class_='thumbnail-img')['src'] 
+	data = []
+
+	for dataBox in soup.find_all('div',class_='card entity-card entity-card-list cf'):
+		titleObg = dataBox.find('h2',class_='meta-title').find('a',class_='meta-title-link').text
+		durationObj = dataBox.find('div',class_='meta-body-item meta-body-info')
+		img_cont = soup.find('img',class_='thumbnail-img')['src'] 
 			
-	# 	data.append({
-	# 		'title':titleObg.strip(),
-	# 		'img_src':img_cont.strip()
-	# 		})
+		data.append({
+			'title':titleObg.strip(),
+			'img_src':img_cont.strip()
+			})
 
-	# return jsonify({'filmes': data})	
+	return jsonify({'filmes': data})	
 
 def main():
 	port = int(os.environ.get("PORT",5000))
