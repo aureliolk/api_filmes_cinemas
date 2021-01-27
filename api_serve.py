274f5ed75a9808e768b645054cde4f9149bfb629
Aurélio Castro <aurelio.cos@outlook.com>
2021-01-27 13:25:12 -0300

    fix caminho soup for img object

--- api_serve.py
+++ api_serve.py
@@ -21,15 +21,12 @@ def filmes():
 	data = []
 
 	for dataBox in soup.find_all('div',class_='card entity-card entity-card-list cf'):
-		titleObg = dataBox.find('h2',class_='meta-title').find('a',class_='meta-title-link').text
-		durationObj = dataBox.find('div',class_='meta-body-item meta-body-info')
-		img_cont = soup.find('img',class_='thumbnail-img')['src'] 
-			
+		titleObg = dataBox.find('h2', class_='meta-title').find('a',class_='meta-title-link')
+		img_cont = dataBox.find('img', class_='thumbnail-img')['src']
 		data.append({
-			'title':titleObg.strip(),
-			'img_src':img_cont.strip()
-			})
-
+		'title':titleObg.text.strip(),
+		'img_src':img_cont.strip()
+		})
 	return jsonify({'filmes': data})	
 
 def main():
